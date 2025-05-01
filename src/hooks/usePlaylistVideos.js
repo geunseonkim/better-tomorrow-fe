5b3ae6e3cd2d21acb7c976a841f5e9eea2ee34cd
jamesjo <jamescakr@gmail.com>
2025-05-01 17:53:44 -0600

    feature: add Multicarousel in MainPage.jsx

--- src/hooks/usePlaylistVideos.js
+++ src/hooks/usePlaylistVideos.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import api from "../utils/api";
+import api from "../utils/youtubeApi";
 
 // const playlistId = "PLoSWVnSA9vG8SK6-_45PAu6RVTaP1zXHf"; //예시
 
