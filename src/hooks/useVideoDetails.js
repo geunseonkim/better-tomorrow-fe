b4feb7831030cfe240daf89c95336e114ca584f9
jamesjo <jamescakr@gmail.com>
2025-05-01 19:38:42 -0600

    fix: change import from api to youtubeApi in hook

--- src/hooks/useVideoDetails.js
+++ src/hooks/useVideoDetails.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import api from "../utils/api";
+import api from "../utils/youtubeApi";
 import { formatDuration, formatDate } from "../utils/dataFormatUtils";
 
 const getVideoDetails = async (videoIds) => {
